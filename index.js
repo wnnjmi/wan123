4e1a34fb31f0a9034d3043b6a61c9e31df50595c
wnnjmi <B022210002@student.utem.edu.my>
2023-10-31 12:05:09 +0800

    first try

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+
 app.use(express.json())
+
 app.get('/', (req, res) => {
  res.send('Hello World!')
 })
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    res.send('Login Success')
+}
+
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
